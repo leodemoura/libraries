401e11fb0571555db9523280eb95eb6548d5c15d
parikshit <parksht@iitk.ac.in>
2014-05-31 16:57:04 +0530

    feat(library/standard/list): add append definition, and related theorems

--- library/standard/list.lean
+++ library/standard/list.lean
@@ -27,6 +27,7 @@ check (cons (succ zero) (cons (succ (succ zero)) nil))
 axiom list_rec {T : Type} {C : list T → Type} (c : C nil) 
     (g : forall x : T, forall l : list T, forall c : C l, C (cons x l)) :
     forall l : list T,  C l
+ 
 
 axiom list_rec_nil {T : Type} {C : list T → Type} (c : C nil) 
     (g : forall x : T, forall l : list T, forall c : C l, C (cons x l)) :
@@ -49,12 +50,11 @@ theorem nil_concat {T : Type} (t : list T) : nil @ t = t
 theorem cons_concat {T : Type} (x : T) (s t : list T) : concat (cons x s) t = cons x (concat s t)
 := list_rec_cons _ _ _ _
 
+
 theorem concat_nil {T : Type} (t : list T) : concat t nil = t
 :=
   list_rec (nil_concat nil) (
-    take x : T, 
-    take l : list T, 
-    assume H : concat l nil = l,
+    fun (x : T) (l : list T) (H : concat l nil = l),
     show concat (cons x l) nil = cons x l, from
       calc
         concat (cons x l) nil = cons x (concat l nil) : cons_concat x l nil
@@ -77,6 +77,7 @@ theorem concat_assoc {T : Type} (s t u : list T) : concat (concat s t) u = conca
          ... = concat (cons x l) (concat t u) : {symm (cons_concat _ _ _)}
   ) s
 
+
 definition length {T : Type} : list T → ℕ
 := list_rec zero (fun x l m, succ m)
 
@@ -86,8 +87,21 @@ theorem length_nil {T : Type} : length (@nil T) = zero
 theorem length_cons {T : Type} (x : T) (t : list T) : length (cons x t) = succ (length t)
 := list_rec_cons _ _ _ _ 
 
--- theorem length_concat {T : Type} (s t : list T) : length (concat s t) = length s + length t
---  := list_rec (length t) (fun (x : T) (l : list T) (c : length l), calc(length (concat (l t)))) s
+ theorem length_concat {T : Type} (s t : list T) : length (concat s t) = length s + length t
+  := list_rec (
+ calc
+ length (concat nil t) = length t : {nil_concat _}
+                   ... = zero + length t : {symm (add_zero_left (length t))}
+                   ... = length (@nil T) +length t : {symm (length_nil)} ) 
+(take x s,
+  assume H : length (concat s t) = length s + length t,
+  calc
+  length (concat (cons x s) t ) = length (cons x (concat s t)) : {cons_concat _ _ _}
+                            ... = succ (length (concat s t))  : {length_cons _ _}
+                            ... = succ (length s + length t)  : { H }
+                            ... = succ (length s) + length t  : {symm (add_succ_left _ _)}
+                            ... = length (cons x s) + length t : {symm (length_cons _ _)} ) s
+
 
 definition reverse {T : Type} : list T → list T := 
   list_rec nil (fun x l r, concat r (cons x nil))
@@ -140,6 +154,88 @@ theorem reverse_reverse {T : Type} (t : list T) : reverse (reverse t) = t
           ... = cons x l : {nil_concat _} 
   ) t
 
+theorem concat_cons {T : Type} (x : T) (s l : list T) : concat s (cons x l) = reverse (concat (reverse l) (cons x (reverse s)))
+:=(
+ calc
+   concat s (cons x l) = concat s (cons x (concat nil l)) : {symm (nil_concat _)}
+                   ... = concat s (concat (cons x nil) l) : {symm (cons_concat _ _ _)}
+                   ... = concat (concat s (cons x nil)) l : {symm (concat_assoc _ _ _)}
+                   ... = concat (concat (reverse(reverse s)) (cons x nil)) l : {symm (reverse_reverse _)}
+                   ... = concat (reverse (cons x (reverse s))) l             : {symm (reverse_cons _ _)}
+                   ... = concat (reverse (cons x (reverse s))) (reverse(reverse l)) : {symm (reverse_reverse _)}
+                   ... = reverse (concat (reverse l) (cons x (reverse s)))         : {symm (reverse_concat _ _)}
+)
+
 
 definition head {T : Type} (null : T) : list T → T 
 := list_rec null (fun x l h, x)
+
+theorem nil_head {T : Type} (null : T) : head null (@nil T) = null
+:= list_rec_nil _ _
+
+
+theorem head_cons {T : Type} (x : T) (null : T) (t : list T) : head null (cons x t) = x
+:= list_rec_cons _ _ _ _
+
+
+definition append {T : Type } (a : T) : list T → list T
+:= list_rec (cons a nil) (fun x l b, cons x b)
+
+
+theorem append_cons {T : Type } (x : T) (a : T) (t : list T) : append a (cons x t)  = cons x (append a t)
+:= list_rec_cons _ _ _ _                                       
+
+theorem nonrec_append {T : Type} (a : T) (t : list T) :append a t = concat t (cons a nil)
+:= 
+   list_rec (
+    calc
+       append a nil = cons a nil             : list_rec_nil _ _
+               ... = concat nil (cons a nil) : symm (nil_concat _)
+ ) (
+    take x l,
+    assume P : append a l = concat l (cons a nil),
+     calc
+      append a (cons x l) = cons x (append a l)          : {append_cons _ _ _}
+                    ... = cons x (concat l (cons a nil)) : { P }
+                    ... = concat (cons x l) (cons a nil) : {symm (cons_concat _ _ _)}
+) t
+
+
+theorem append_reverse {T : Type} (a : T) (t : list T) : append a t = reverse (cons a (reverse t))
+:= 
+   list_rec (
+    calc 
+     append a nil = cons a nil                        : (list_rec_nil _ _)
+             ... = concat nil (cons a nil)            : {symm (nil_concat _)}
+             ... = concat (reverse nil) (cons a nil)  : {symm (reverse_nil)}    
+             ... = reverse (cons a nil)               : {symm (reverse_cons _ _)}
+             ... = reverse (cons a (reverse nil))     : {symm (reverse_nil)}
+) (
+       take x l,
+       assume H : append a l = reverse (cons a (reverse l)),
+        calc
+         append a (cons x l) = concat (cons x l) (cons a nil)                    : nonrec_append _ _
+                        ... = concat (reverse (reverse (cons x l))) (cons a nil) : {symm (reverse_reverse _)}
+                        ... = reverse (cons a (reverse (cons x l)))              : {symm (reverse_cons _ _)}
+) t
+
+
+theorem head_concat {T : Type} (s t : list T) (null : T) : (¬(s=nil)) → (head null (concat s t) = head null s)
+:= 
+   list_rec (take H : ¬ (nil=nil), absurd_elim (head null (concat nil t) = head null nil) (refl nil) H) 
+   ( take x s,
+     assume C :  ¬ (s = nil) → head null (s @ t) = head null s,
+     take H : ¬ (cons x s = nil),
+     calc
+       head null (concat (cons x s) t) = head null (cons x (concat s t)) : {cons_concat _ _ _}
+                                 ... = x                                 : {head_cons _ _ _}
+                                  ... = head null (cons x s)             : {symm ( head_cons x null s)}
+   ) s
+
+
+                      
+              
+  
+
+
+    
