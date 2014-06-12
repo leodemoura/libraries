14b1664a2cf7eddf668f1f2b7ab7c0b0c819bdc3
Jeremy Avigad <avigad@cmu.edu>
2014-06-12 20:04:53 +0200

    added facts about div and mod

--- library/standard/nat2.lean
+++ library/standard/nat2.lean
@@ -14,16 +14,41 @@ import find
 import tactic
 
 using nat
-
 unary_nat
 
---
--- some stuff for nat
---
+namespace nat
 
-add_rewrite add_zero_left add_zero_right
-add_rewrite mul_zero_left mul_zero_right
+
+-- some stuff for nat
+-- ==================
+
+-- suggestions:
+-- 
+-- zero_lt ~~> succ_lt_zero
+-- le_zero ~~> zero_le
+-- add_left_inj ~~> add_left_cancel etc.
+
 add_rewrite succ_ne_zero
+add_rewrite add_zero_left add_zero_right
+add_rewrite add_succ_left add_succ_right
+add_rewrite add_comm add_assoc add_comm_left
+add_rewrite le_add_right le_add_left 
+
+add_rewrite mul_zero_left mul_zero_right
+add_rewrite mul_succ_left mul_succ_right
+add_rewrite mul_comm mul_assoc mul_comm_left
+
+check mul_assoc
+check mul_comm_right
+check mul_comm_left
+
+add_rewrite sub_add_left
+
+theorem add_simp_test (x y z w : ℕ) : (x + w) + (z + y) = z + (y + x) + w
+:= by simp
+
+theorem add_simp_test2 (x y z w : ℕ) : z * y + x * w = w * x + y * z
+:= by simp
 
 theorem complete_induction_on {P : nat → Bool} (a : nat)
     (H : ∀(n : nat), (∀m, m < n → P m) → P n) : P a
@@ -251,9 +276,26 @@ theorem div_rec {x y : ℕ} (H1 : y > 0) (H2 : x ≥ y) : x div y = succ ((x - y
         or_elim H4
           (assume H5 : y = 0, not_elim (lt_irrefl _) (subst H1 H5))
           (assume H5 : x < y, not_elim (lt_le_antisym H5) H2)),
-  trans (div_aux_spec _ _) (not_imp_if_eq H3 _ _) 
+  trans (div_aux_spec _ _) (not_imp_if_eq H3 _ _)
 
 add_rewrite div_zero div_less zero_div
+
+theorem div_add_self (x : ℕ) {z : ℕ} (H : z > 0) : (x + z) div z = succ (x div z)
+:=
+  have H1 : z ≤ x + z, by simp,
+  let H2 := div_rec H H1 in
+  by simp
+
+theorem div_add_mul_self (x y : ℕ) {z : ℕ} (H : z > 0) : (x + y * z) div z = x div z + y
+:=
+  induction_on y (by simp)
+    (take y, 
+      assume IH : (x + y * z) div z = x div z + y,
+      calc
+        (x + succ y * z) div z = (x + y * z + z) div z : by simp
+          ... = succ ((x + y * z) div z) : div_add_self _ H
+          ... = x div z + succ y : by simp)
+
 
 -- the definition of mod
 -- ---------------------
@@ -313,12 +355,28 @@ theorem mod_rec {x y : ℕ} (H1 : y > 0) (H2 : x ≥ y) : x mod y = (x - y) mod 
           (assume H5 : x < y, not_elim (lt_le_antisym H5) H2)),
   trans (mod_aux_spec _ _) (not_imp_if_eq H3 _ _) 
 
+theorem mod_add_self (x : ℕ) {z : ℕ} (H : z > 0) : (x + z) mod z = x mod z
+:=
+  have H1 : z ≤ x + z, by simp,
+  let H2 := mod_rec H H1 in
+  by simp
+
+theorem mod_add_mul_self (x y : ℕ) {z : ℕ} (H : z > 0) : (x + y * z) mod z = x mod z
+:=
+  induction_on y (by simp)
+    (take y, 
+      assume IH : (x + y * z) mod z = x mod z,
+      calc
+        (x + succ y * z) mod z = (x + y * z + z) mod z : by simp
+          ... = (x + y * z) mod z : mod_add_self _ H
+          ... = x mod z : IH)
+
 add_rewrite mod_zero mod_less zero_mod
 
 -- properties of div and mod together
 -- ----------------------------------
 
-theorem mod_lt (x y : ℕ) (H : y > 0) : x mod y < y
+theorem mod_lt (x : ℕ) {y : ℕ} (H : y > 0) : x mod y < y
 :=
   complete_induction'_on x
     (show 0 mod y < y, from subst H (symm (zero_mod _)))
@@ -375,6 +433,74 @@ theorem div_mod_eq (x y : ℕ) : x = x div y * y + x mod y
 theorem mod_le (x y : ℕ) : x mod y ≤ x
 := subst (le_add_left (x mod y) _) (symm (div_mod_eq _ _))
 
+axiom sorry {P : Bool} : P 
+
+-- a good example where simplifying using the context causes problems
+theorem remainder_unique {y : ℕ} (H : y > 0) {q1 r1 q2 r2 : ℕ} (H1 : r1 < y) (H2 : r2 < y) 
+    (H3 : q1 * y + r1 = q2 * y + r2) : r1 = r2
+:=
+  calc
+    r1 = r1 mod y : by simp
+      ... = (r1 + q1 * y) mod y : symm (mod_add_mul_self _ _ H)
+      ... = (q1 * y + r1) mod y : {add_comm _ _}
+      ... = (r2 + q2 * y) mod y : by simp
+      ... = r2 mod y : mod_add_mul_self _ _ H
+      ... = r2 : by simp
+
+theorem quotient_unique {y : ℕ} (H : y > 0) {q1 r1 q2 r2 : ℕ} (H1 : r1 < y) (H2 : r2 < y) 
+    (H3 : q1 * y + r1 = q2 * y + r2) : q1 = q2
+:=
+  have H4 : q1 * y + r2 = q2 * y + r2, from subst H3 (remainder_unique H H1 H2 H3),
+  have H5 : q1 * y = q2 * y, from add_left_inj H4,
+  have H6 : y > 0, from le_lt_trans (le_zero _) H1,
+  show q1 = q2, from mul_right_inj H6 H5
+
+theorem div_mul_mul {z : ℕ} (x y : ℕ) (zpos : z > 0) : (z * x) div (z * y) = x div y
+:=
+  by_cases (y = 0)
+    (assume H : y = 0, by simp)
+    (assume H : y ≠ 0, 
+      have ypos : y > 0, from ne_zero_positive H,
+      have zypos : z * y > 0, from mul_positive zpos ypos,
+      have H1 : (z * x) mod (z * y) < z * y, from mod_lt _ zypos,
+      have H2 : z * (x mod y) < z * y, from mul_lt_left zpos (mod_lt _ ypos),
+      quotient_unique zypos H1 H2
+        (calc
+          ((z * x) div (z * y)) * (z * y) + (z * x) mod (z * y) = z * x : symm (div_mod_eq _ _)
+            ... = z * (x div y * y + x mod y) : {div_mod_eq _ _}
+            ... = z * (x div y * y) + z * (x mod y) : mul_add_distr_right _ _ _
+            ... = (x div y) * (z * y) + z * (x mod y) : {mul_comm_left _ _ _}))
+-- something wrong with the term order 
+--            ... = (x div y) * (z * y) + z * (x mod y) : by simp))
+
+theorem mod_mul_mul {z : ℕ} (x y : ℕ) (zpos : z > 0) : (z * x) mod (z * y) = z * (x mod y)
+:=
+  by_cases (y = 0)
+    (assume H : y = 0, by simp)
+    (assume H : y ≠ 0, 
+      have ypos : y > 0, from ne_zero_positive H,
+      have zypos : z * y > 0, from mul_positive zpos ypos,
+      have H1 : (z * x) mod (z * y) < z * y, from mod_lt _ zypos,
+      have H2 : z * (x mod y) < z * y, from mul_lt_left zpos (mod_lt _ ypos),
+      remainder_unique zypos H1 H2
+        (calc
+          ((z * x) div (z * y)) * (z * y) + (z * x) mod (z * y) = z * x : symm (div_mod_eq _ _)
+            ... = z * (x div y * y + x mod y) : {div_mod_eq _ _}
+            ... = z * (x div y * y) + z * (x mod y) : mul_add_distr_right _ _ _
+            ... = (x div y) * (z * y) + z * (x mod y) : {mul_comm_left _ _ _}))
+
+theorem mod_1 (x : ℕ) : x mod 1 = 0
+:=
+  have H1 : x mod 1 < 1, from mod_lt _ (lt_zero 0),
+  le_zero_inv (lt_succ_le H1)
+
+add_rewrite mod_1
+
+theorem div_1 (x : ℕ) : x div 1 = x
+:=
+  symm(calc
+    x = x div 1 * 1 + x mod 1 : div_mod_eq _ _
+     ... = x div 1 : by simp)
 
 -- divides
 -- =======
@@ -415,7 +541,7 @@ theorem mul_eq_imp_dvd {z x y : ℕ} (H : z * y = x) :  y | x
             ... = 0 : xz)
       (assume ynz : y ≠ 0,
         have ypos : y > 0, from ne_zero_positive ynz,
-        have H3 : (z - x div y) * y < y, from subst (mod_lt x y ypos) (symm H2),
+        have H3 : (z - x div y) * y < y, from subst (mod_lt x ypos) (symm H2),
         have H4 : (z - x div y) * y < 1 * y, from subst H3 (symm (mul_one_left y)),
         have H5 : z - x div y < 1, from mul_lt_right_inv H4,
         have H6 : z - x div y = 0, from le_zero_inv (lt_succ_le H5),
@@ -466,7 +592,7 @@ theorem gcd_aux_decreasing (g : ℕ ## ℕ → ℕ) (m : ℕ) (p : ℕ ## ℕ) (
       (assume H1 : y ≠ 0,
         have ypos : y > 0, from ne_zero_positive H1,
         have H2 : gcd_aux_measure p' = x mod y, from tproj2_tpair _ _,
-        have H3 : gcd_aux_measure p' < gcd_aux_measure p, from subst (mod_lt _ _ ypos) (symm H2), 
+        have H3 : gcd_aux_measure p' < gcd_aux_measure p, from subst (mod_lt _ ypos) (symm H2), 
         have H4: gcd_aux_measure p' < m, from lt_le_trans H3 H,
         symm (calc
           rhs = restrict 0 gcd_aux_measure g m p' : not_imp_if_eq H1 _ _
@@ -492,10 +618,6 @@ theorem gcd_def (x y : ℕ) : gcd x y = if y = 0 then x else gcd y (x mod y)
 
 theorem gcd_zero (x : ℕ) : gcd x 0 = x
 := trans (gcd_def x 0) (imp_if_eq (refl _) _ _)
-
--- what is a better way to do this?
-theorem succ_ne_zero' (x : ℕ) : (succ x = 0) = false
-:= eqf_intro (succ_ne_zero x)
 
 add_rewrite gcd_zero 
 
